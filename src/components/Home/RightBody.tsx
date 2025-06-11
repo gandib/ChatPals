310af2386245622f97961570ee694a37bc1c078c
gandib <55914763+gandib@users.noreply.github.com>
2025-06-12 00:24:12 +0600

    Right sidebar body completed

--- src/components/Home/RightBody.tsx
+++ src/components/Home/RightBody.tsx
@@ -1,89 +1,54 @@
+import { getUserChat } from "../../redux/features/message/messageSlice";
+import { useAppSelector } from "../../redux/hooks";
+
 const RightBody = () => {
+  const userChat = useAppSelector(getUserChat);
+
+  // Extract all non-null chat images
+  const chatImages = userChat?.chats?.filter((chat) => chat.image) ?? [];
+
   return (
     <div>
       <h1 className="border-2 border-b-gray-100 border-t-0 border-x-0 p-2 text-blue-500 font-semibold">
         Images
       </h1>
 
-      {/* Display images  */}
+      {/* Display dynamic chat images only */}
       <div className="grid grid-cols-3 gap-4 p-4">
-        <div className="">
-          <img
-            src="https://connectme-html.themeyn.com/images/gallery/chat/thumb-1.jpg"
-            alt=""
-            className="w-24 h-24  rounded-lg"
-          />
-        </div>
-        <div className="">
-          <img
-            src="https://connectme-html.themeyn.com/images/gallery/chat/2.jpg"
-            alt=""
-            className="w-24 h-24  rounded-lg"
-          />
-        </div>
-        <div className="">
-          <img
-            src="https://connectme-html.themeyn.com/images/gallery/chat/3.jpg"
-            alt=""
-            className="w-24 h-24  rounded-lg"
-          />
-        </div>
-        <div className="">
-          <img
-            src="https://connectme-html.themeyn.com/images/gallery/chat/4.jpg"
-            alt=""
-            className="w-24 h-24  rounded-lg"
-          />
-        </div>
-        <div className="">
-          <img
-            src="https://connectme-html.themeyn.com/images/gallery/chat/5.jpg"
-            alt=""
-            className="w-24 h-24  rounded-lg"
-          />
-        </div>
-        <div className="">
-          <img
-            src="https://connectme-html.themeyn.com/images/gallery/chat/6.jpg"
-            alt=""
-            className="w-24 h-24  rounded-lg"
-          />
-        </div>
-        <div className="">
-          <img
-            src="https://connectme-html.themeyn.com/images/gallery/chat/thumb-1.jpg"
-            alt=""
-            className="w-24 h-24  rounded-lg"
-          />
-        </div>
-        <div className="">
-          <img
-            src="https://connectme-html.themeyn.com/images/gallery/chat/2.jpg"
-            alt=""
-            className="w-24 h-24  rounded-lg"
-          />
-        </div>
-        <div className="">
-          <img
-            src="https://connectme-html.themeyn.com/images/gallery/chat/thumb-1.jpg"
-            alt=""
-            className="w-24 h-24  rounded-lg"
-          />
-        </div>
-        <div className="">
-          <img
-            src="https://connectme-html.themeyn.com/images/gallery/chat/thumb-1.jpg"
-            alt=""
-            className="w-24 h-24  rounded-lg"
-          />
-        </div>
-        <div className="">
-          <img
-            src="https://connectme-html.themeyn.com/images/gallery/chat/thumb-1.jpg"
-            alt=""
-            className="w-24 h-24  rounded-lg"
-          />
-        </div>
+        {chatImages.map((chat) => (
+          <div key={chat._id}>
+            <img
+              src={chat.image}
+              alt="chat"
+              className="w-24 h-24 rounded-lg object-cover"
+            />
+          </div>
+        ))}
+
+        {/* Display sample/static images if no chat images exist */}
+        {chatImages.length === 0 && (
+          <>
+            {[
+              "1.jpg",
+              "2.jpg",
+              "3.jpg",
+              "4.jpg",
+              "5.jpg",
+              "6.jpg",
+              "thumb-1.jpg",
+              "3.jpg",
+              "2.jpg",
+            ].map((img, idx) => (
+              <div key={idx}>
+                <img
+                  src={`https://connectme-html.themeyn.com/images/gallery/chat/${img}`}
+                  alt={`sample-${idx}`}
+                  className="w-24 h-24 rounded-lg object-cover"
+                />
+              </div>
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
